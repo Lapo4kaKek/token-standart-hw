47adaecc22acada7a406cc81810db2d466a52485
Lapo4kaKek <Sheydos2003@yandex.ru>
2024-10-19 17:57:36 +0000

    update hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,27 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_URL || "";
+
+const alchemy_key = process.env.ALCHEMY_TOKEN || "";
+
+const URL = `${SEPOLIA_RPC_URL}${alchemy_key}`;
+
+const privateKey = process.env.PRIVATE_KEY || "";
+
+console.log(`URL: ${URL}}`)
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.27",
+  solidity: "0.8.20",
+  networks: {
+    sepolia: {
+      url: URL,
+      accounts: [privateKey],
+    }
+  }
 };
 
 export default config;
